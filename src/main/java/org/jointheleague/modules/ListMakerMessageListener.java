378263c0b865cd368c3cd2cc8ef20d8c088e51d7
root <root@041b8cee77a2>
2020-06-14 18:26:31 +0000

    Working on commands

--- src/main/java/org/jointheleague/modules/ListMakerMessageListener.java
+++ src/main/java/org/jointheleague/modules/ListMakerMessageListener.java
@@ -1,6 +1,12 @@
 package org.jointheleague.modules;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.javacord.api.event.message.MessageCreateEvent;
 
@@ -8,7 +14,7 @@ import net.aksingh.owmjapis.api.APIException;
 
 public class ListMakerMessageListener extends CustomMessageCreateListener{
 
-	final String New_List_Command = "!makeList";
+	final String Show_List_Command = "!ShowList";
 	final String Remove_Command = "!removeFromList";
 	final String Add_Command = "!addToList";
 	final String Commands = "!commands";
@@ -20,24 +26,26 @@ public class ListMakerMessageListener extends CustomMessageCreateListener{
 
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
-		if (event.getMessageContent().equals(New_List_Command)) {
-			HandleMakeList(event);
+		if (event.getMessageContent().equals(Show_List_Command)) {
+			HandleShowList(event);
 		}else if (event.getMessageContent().equals(Remove_Command)) {
 			HandleRemove(event);
 		}else if (event.getMessageContent().equals(Add_Command)) {
 			HandleAdd(event);
 		}else if (event.getMessageContent().equals(Commands)) {
 			event.getChannel().sendMessage("The commands are:\n" + 
-					New_List_Command + ":Creates a new lists \n" +
-					Remove_Command + ":Removes from the list at number place directly after command(ex: !removeFromList 5) \n" + 
+					Show_List_Command + ":Shows the created list \n" +
+					Remove_Command + ":Removes task from top of the list \n" + 
 					Add_Command + ":Adds typed string to bottom of the list");
 		}
 	}
 
 	private void HandleAdd(MessageCreateEvent event) {
 	try {
+		String test = event.getMessageContent().substring(11);
 		FileWriter fw = new FileWriter("src/main/java/org.jointheleague.modules/list.txt");
-		fw.write("..." + "\n");
+		fw.write(test + "\n");
+		fw.close();
 	} catch (Exception e) {
 	
 	}
@@ -45,12 +53,36 @@ public class ListMakerMessageListener extends CustomMessageCreateListener{
 	}
 
 	private void HandleRemove(MessageCreateEvent event) {
-		
+		try {
+			String remove = event.getMessageContent().substring(15);
+			BufferedReader read = new BufferedReader(new FileReader("src/main/java/org.jointheleague.modules/list.txt"));
+			FileWriter write = new FileWriter("src/main/java/org.jointheleague.modules/temp.txt", true);
+			String line = read.readLine();
+			while (line!=null && remove != line) {
+				write.write(line + "\n");
+				}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
-	private void HandleMakeList(MessageCreateEvent event) {
-		
+	private void HandleShowList(MessageCreateEvent event) {
+		try {
+			String test = "";
+			BufferedReader br = new BufferedReader(new FileReader("src/main/java/org.jointheleague.modules/list.txt"));
+			String stuff = br.readLine();
+			//while (stuff!=null) {
+			//test += br.readLine()+ "\n";
+			//}
+		event.getChannel().sendMessage("src/main/java/org.jointheleague.modules/list.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
