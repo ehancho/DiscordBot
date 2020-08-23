d4e226d38a2c87d5be63f9903add85d11140b494
root <root@5bd5b90fc60e>
2020-08-23 18:30:02 +0000

    Won't enter split if statement for some reason, always goes to following else statement

--- src/main/java/org/jointheleague/modules/Chopsticks.java
+++ src/main/java/org/jointheleague/modules/Chopsticks.java
@@ -36,82 +36,87 @@ public class Chopsticks extends CustomMessageCreateListener {
 			String hand = event.getMessageContent().substring(11);
 			HandleAdd(hand, event);
 			botTurn = true;
-		}else if (event.getMessageContent().startsWith(split_command) && playing == true) {
+		} else if (event.getMessageContent().startsWith(split_command) && playing == true) {
 			String ouch = event.getMessageContent().substring(11);
 			HandleSplit(ouch, event);
 		}
-		if(botTurn == true) {
+		if (botTurn == true) {
 			HandleBotPlay(event);
 			botTurn = false;
 		}
-		if(hand1.length()+hand2.length()==0&&playing==true) {
+		if (hand1.length() + hand2.length() == 0 && playing == true) {
 			event.getChannel().sendMessage("Bot Wins");
 			playing = false;
 		}
-		if(botHand1.length()+botHand2.length()==0 &&playing==true) {
+		if (botHand1.length() + botHand2.length() == 0 && playing == true) {
 			event.getChannel().sendMessage("Player Wins");
 			playing = false;
 		}
-		if(playing == false) {
+		if (playing == false) {
 			hand1 = "I";
 			hand2 = "I";
 			botHand1 = "I";
 			botHand2 = "I";
-			
+
 		}
-	
+
 	}
 
 	private void HandleSplit(String str, MessageCreateEvent event) {
 		// TODO Auto-generated method stub
 		String[] move = str.split(" ");
 		int num = Integer.parseInt(move[0]);
-		if(move[1]=="left") {
-			if(hand2.length() >= num && num+hand1.length() < 5) {
+		System.out.println(move[1]);
+		if (move[1] == "right") {
+			if (hand1.length() > num) {
+				event.getChannel().sendMessage("test2");
+
 				String temp = "";
 				for (int i = 0; i < num; i++) {
-					temp+="I";
+					temp += "I";
 				}
-				hand1 += temp;
-				String removal = "";
-				int number = hand2.length()-num;
+				hand2 += temp;
+				hand1 = hand1.substring(0, hand1.length()-temp.length());
+				/*int number = hand1.length() - temp.length();
 				for (int i = 0; i < number; i++) {
-					removal+="I";
+					remove += "I";
 				}
-				hand2 = removal;
+				hand1 = remove;*/
 			}
-		}else if(move[1]=="right") {
-			
-		}else {
+		} else {
 			event.getChannel().sendMessage("invalid message");
+
 		}
+		event.getChannel().sendMessage("Bot:   " + botHand1 + "     " + botHand2);
+		event.getChannel().sendMessage("You:   " + hand1 + "     " + hand2);
+
 	}
 
 	private void HandleBotPlay(MessageCreateEvent event) {
 		// TODO Auto-generated method stub
-		if(hand1.length()>=hand2.length()) {
-			if (botHand1.length()>=botHand2.length()) {
+		if (hand1.length() >= hand2.length()) {
+			if (botHand1.length() >= botHand2.length()) {
 				hand1 += botHand1;
-			}else {
-				hand1+= botHand2;
+			} else {
+				hand1 += botHand2;
 			}
-		}else {
-			if(botHand1.length()>=botHand2.length()) {
-				hand2+=botHand1;
-			}else {
-				hand2+=botHand2;
+		} else {
+			if (botHand1.length() >= botHand2.length()) {
+				hand2 += botHand1;
+			} else {
+				hand2 += botHand2;
 			}
 		}
-		if(hand1.length()>=5) {
+		if (hand1.length() >= 5) {
 			hand1 = "";
 		}
-		if(hand2.length()>=5) {
+		if (hand2.length() >= 5) {
 			hand2 = "";
 		}
-		if(botHand1.length()>=5) {
+		if (botHand1.length() >= 5) {
 			botHand1 = "";
 		}
-		if(botHand2.length()>=5) {
+		if (botHand2.length() >= 5) {
 			botHand2 = "";
 		}
 		event.getChannel().sendMessage("Bot:   " + botHand1 + "     " + botHand2);
@@ -140,16 +145,16 @@ public class Chopsticks extends CustomMessageCreateListener {
 		} else {
 			event.getChannel().sendMessage("invalid message");
 		}
-		if(hand1.length()>=5) {
+		if (hand1.length() >= 5) {
 			hand1 = "";
 		}
-		if(hand2.length()>=5) {
+		if (hand2.length() >= 5) {
 			hand2 = "";
 		}
-		if(botHand1.length()>=5) {
+		if (botHand1.length() >= 5) {
 			botHand1 = "";
 		}
-		if(botHand2.length()>=5) {
+		if (botHand2.length() >= 5) {
 			botHand2 = "";
 		}
 		event.getChannel().sendMessage("Bot:   " + botHand1 + "     " + botHand2);
